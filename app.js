c4f435836539461a8b74238a4c11183ae11d7ef0
iuri matcharashvili <119754002+iurimach@users.noreply.github.com>
2023-04-11 22:25:35 +0400

    version 2

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+
+
+var root=document.querySelector("#root")
+root.style.display="none";
+
+function openpage(){
+  
+  root.style.display="block";
+  var rootheader=document.querySelector("#rootheader").style.marginTop = "0px";
+  var openbox=document.querySelector("#openbox")
+  openbox.style.display="none"
+  var checkbox=document.querySelector("#checkbox").style.display="none"
+  let label1=document.querySelector("#label1").style.display="none"
+
+
+}
+
+ 
+var checkbox =document.querySelector("#checkbox")
+checkbox.addEventListener('change', (event) =>{
+      if (event.target.checked){
+        openbox.style.display="inline-block"
+        openbox.disabled = false
+        openbox.style.opacity= 1;
+        openbox.style.border=" 2px solid green"
+      }
+      else{
+        openbox.disabled = true  
+        openbox.style.border=" 2px solid red"                   
+      }
+  })
+
+
+
+var openbox=document.querySelector("#openbox")
+
+
 function darck(){
 
 if (document.body.style.backgroundColor = "black"){
